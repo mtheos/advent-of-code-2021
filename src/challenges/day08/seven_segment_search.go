affde76a237a45bbbf4083050c9129a5bdd1dccf
Michael Theos <michael@theos.me>
2021-12-10 01:23:18 +1100

    day 08

--- src/challenges/day08/seven_segment_search.go
+++ src/challenges/day08/seven_segment_search.go
@@ -8,22 +8,32 @@ import (
 	"strings"
 )
 
-var segmentLUT = []int{
-	1: 2,
-	7: 3,
-	4: 4,
-	2: 5,
-	3: 5,
-	5: 5,
-	0: 6,
-	6: 6,
-	9: 6,
-	8: 7,
-}
-
 type Display struct {
 	inputs  [10]string
 	outputs [4]string
+}
+
+const (
+	T byte = iota
+	TL
+	TR
+	M
+	BL
+	BR
+	B
+)
+
+var segmentCountLUT = []int{
+	0: 6,
+	1: 2,
+	2: 5,
+	3: 5,
+	4: 4,
+	5: 5,
+	6: 6,
+	7: 3,
+	8: 7,
+	9: 6,
 }
 
 func readInput(fileName string) []Display {
@@ -63,20 +73,20 @@ func putFirstEmpty(arr *[3]string, input string) {
 	panic("panic")
 }
 
-func fn(inputs [10]string) ([4]string, [3]string, [3]string) {
+func parseInput(inputs [10]string) ([4]string, [3]string, [3]string) {
 	var length5 [3]string
 	var length6 [3]string
 	var known [4]string
 
 	for _, input := range inputs {
 		switch len(input) {
-		case segmentLUT[1]:
+		case segmentCountLUT[1]:
 			known[0] = input
-		case segmentLUT[4]:
+		case segmentCountLUT[4]:
 			known[1] = input
-		case segmentLUT[7]:
+		case segmentCountLUT[7]:
 			known[2] = input
-		case segmentLUT[8]:
+		case segmentCountLUT[8]:
 			known[3] = input
 		default:
 			if len(input) == 5 {
@@ -115,36 +125,54 @@ func fn(inputs [10]string) ([4]string, [3]string, [3]string) {
  6666    6666    6666
 */
 
-func solveIt(display Display) int {
+func solveIt(display Display, resultChan chan int) {
 	var segments [7]string
-	known, length5, length6 := fn(display.inputs)
+	known, length5, length6 := parseInput(display.inputs)
 	one, four, seven, eight := known[0], known[1], known[2], known[3]
 
 	fillSegmentZero(seven, one, &segments)
-	fillSegmentOne(length5, four, &segments)
-	fillSegmentTwo(length6, one, &segments)
-	fillSegmentFour(length5, one, &segments)
+	fillSegmentsOneThreeFour(length5, four, &segments)
+	fillSegmentsTwoAndFive(length6, one, &segments)
+	fillSegmentSix(eight, &segments)
+	LUT := createLUT(segments)
+	result := 0
+	multiplier := 1000
+	for _, digit := range display.outputs {
+		result += LUT[StrSort(digit)] * multiplier
+		multiplier /= 10
+	}
+	resultChan <- result
+}
 
-	zero := ""
-	fillSegmentThree(zero, eight, &segments)
-	fillSegmentFive(&segments)
-	fillSegmentSix(&segments)
-
-	return 0
+func createLUT(segments [7]string) map[string]int {
+	digits := make(map[string]int)
+	digits[StrSort(StrCat(segments[T], segments[TL], segments[TR], segments[BL], segments[BR], segments[B]))] = 0
+	digits[StrSort(StrCat(segments[TR], segments[BR]))] = 1
+	digits[StrSort(StrCat(segments[T], segments[TR], segments[M], segments[BL], segments[B]))] = 2
+	digits[StrSort(StrCat(segments[T], segments[TR], segments[M], segments[BR], segments[B]))] = 3
+	digits[StrSort(StrCat(segments[TL], segments[TR], segments[M], segments[BR]))] = 4
+	digits[StrSort(StrCat(segments[T], segments[TL], segments[M], segments[BR], segments[B]))] = 5
+	digits[StrSort(StrCat(segments[T], segments[TL], segments[M], segments[BL], segments[BR], segments[B]))] = 6
+	digits[StrSort(StrCat(segments[T], segments[TR], segments[BR]))] = 7
+	digits[StrSort(StrCat(segments[T], segments[TL], segments[TR], segments[M], segments[BL], segments[BR], segments[B]))] = 8
+	digits[StrSort(StrCat(segments[T], segments[TL], segments[TR], segments[M], segments[BR], segments[B]))] = 9
+	return digits
 }
 
 func fillSegmentZero(seven string, one string, segments *[7]string) {
 	for _, seg := range seven {
 		segment := string(seg)
 		if !strings.Contains(one, segment) {
-			segments[0] = segment
+			segments[T] = segment
 			return
 		}
 	}
-	panic("panic")
+	if segments[T] == "" {
+		panic("panic")
+	}
 }
 
-func fillSegmentOne(length5 [3]string, four string, segments *[7]string) {
+func fillSegmentsOneThreeFour(length5 [3]string, four string, segments *[7]string) {
 	opts := make(map[string]int)
 	for _, number := range length5 {
 		for _, seg := range number {
@@ -154,60 +182,46 @@ func fillSegmentOne(length5 [3]string, four string, segments *[7]string) {
 	}
 	for key, segCount := range opts {
 		if segCount == 1 && strings.Contains(four, key) {
-			segments[1] = key
-			return
+			segments[TL] = key
+		} else if segCount == 3 && strings.Contains(four, key) {
+			segments[M] = key
+		} else if segCount == 1 && !strings.Contains(four, key) {
+			segments[BL] = key
 		}
 	}
-	panic("panic")
-}
-
-func fillSegmentTwo(length6 [3]string, one string, segments *[7]string) {
-	for _, number := range length6 {
-		for _, seg := range number {
-			segment := string(seg)
-			if strings.Contains(one, segment) {
-				segments[2] = segment
-				return
-			}
-		}
+	if segments[TL] == "" || segments[M] == "" || segments[BL] == "" {
+		panic("panic")
 	}
-	panic("panic")
 }
 
-func fillSegmentThree(zero string, eight string, segments *[7]string) {
-	for _, seg := range eight {
-		segment := string(seg)
-		if !strings.Contains(zero, segment) {
-			segments[3] = segment
-			return
-		}
-	}
-	panic("panic")
-}
-
-func fillSegmentFour(length5 [3]string, one string, segments *[7]string) {
+func fillSegmentsTwoAndFive(length6 [3]string, one string, segments *[7]string) {
 	opts := make(map[string]int)
-	for _, number := range length5 {
+	for _, number := range length6 {
 		for _, seg := range number {
 			segment := string(seg)
 			opts[segment]++
 		}
 	}
 	for key, segCount := range opts {
-		if segCount == 1 && !strings.Contains(one, key) {
-			segments[4] = key
-			return
+		if segCount == 2 && strings.Contains(one, key) {
+			segments[TR] = key
+		} else if segCount == 3 && strings.Contains(one, key) {
+			segments[BR] = key
 		}
 	}
-	panic("panic")
+	if segments[TR] == "" || segments[BR] == "" {
+		panic("panic")
+	}
 }
 
-func fillSegmentFive(segments *[7]string) {
-	panic("panic")
-}
-
-func fillSegmentSix(segments *[7]string) {
-	panic("panic")
+func fillSegmentSix(eight string, segments *[7]string) {
+	for _, segment := range segments {
+		eight = strings.Replace(eight, segment, "", 1)
+	}
+	segments[B] = eight
+	if segments[B] == "" {
+		panic("panic")
+	}
 }
 
 func ezMode(input []Display, ch chan<- int) {
@@ -215,13 +229,13 @@ func ezMode(input []Display, ch chan<- int) {
 	for _, display := range input {
 		for _, output := range display.outputs {
 			switch len(output) {
-			case segmentLUT[1]:
+			case segmentCountLUT[1]:
 				fallthrough
-			case segmentLUT[4]:
+			case segmentCountLUT[4]:
 				fallthrough
-			case segmentLUT[7]:
+			case segmentCountLUT[7]:
 				fallthrough
-			case segmentLUT[8]:
+			case segmentCountLUT[8]:
 				count++
 			default:
 			}
@@ -231,10 +245,15 @@ func ezMode(input []Display, ch chan<- int) {
 }
 
 func hardMode(input []Display, ch chan<- int) {
+	resultChan := make(chan int)
 	for _, display := range input {
-		solveIt(display)
+		go solveIt(display, resultChan)
 	}
-	ch <- 0
+	sum := 0
+	for i := 0; i < len(input); i++ {
+		sum += <-resultChan
+	}
+	ch <- sum
 }
 
 func Go(fileName string, ch chan string) {
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,7 +1,26 @@
 package utils
 
+import (
+	"sort"
+	"strings"
+)
+
 func MaybePanic(err interface{}) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func StrSort(s string) string {
+	x := strings.Split(s, "")
+	sort.Strings(x)
+	return strings.Join(x, "")
+}
+
+func StrCat(stringsArr ...string) string {
+	sb := strings.Builder{}
+	for _, s := range stringsArr {
+		sb.WriteString(s)
+	}
+	return sb.String()
 }
