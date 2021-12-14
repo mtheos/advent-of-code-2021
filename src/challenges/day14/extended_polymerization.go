12a6c713ff4f96d2ae32205ac17a9cbfbf3ada4e
Michael Theos <michael@theos.me>
2021-12-14 23:01:10 +1100

    day 14

--- src/challenges/day14/extended_polymerization.go
+++ src/challenges/day14/extended_polymerization.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-func readInput(fileName string) (string, map[string]uint8) {
+func readInput(fileName string) (string, map[uint16]uint8) {
 	file, err := os.Open(fileName)
 	MaybePanic(err)
 	defer func(file *os.File) {
@@ -20,46 +20,48 @@ func readInput(fileName string) (string, map[string]uint8) {
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
 	polymer := scanner.Text()
-	transformations := make(map[string]uint8)
+	transformations := make(map[uint16]uint8)
 	scanner.Scan()
 	scanner.Text() // blank line
 	for scanner.Scan() {
 		split := strings.Split(scanner.Text(), " -> ")
-		transformations[split[0]] = split[1][0]
+		// This is as obtuse as I could be while being reasonable
+		pair := uint16(split[0][0])<<8 + uint16(split[0][1])
+		transformations[pair] = split[1][0]
 	}
 	return polymer, transformations
 }
 
-func countPolymers(chain string) map[string]int {
-	polymers := make(map[string]int)
+func countPolymers(chain string) map[uint16]int64 {
+	polymers := make(map[uint16]int64)
 	for i := 0; i < len(chain)-1; i++ {
-		pair := chain[i : i+2]
+		pair := uint16(chain[i])<<8 + uint16(chain[i+1])
 		polymers[pair]++
 	}
 	return polymers
 }
 
 // Not sure if this actually keeps track of enough information to recreate the polymer /shrug
-func polymerizeButWithDP(polymers map[string]int, transformations map[string]uint8) map[string]int {
-	nextPolymers := make(map[string]int)
-	for k, v := range polymers {
-		monomer, ok := transformations[k]
+func polymerizeButWithDP(polymers map[uint16]int64, transformations map[uint16]uint8) map[uint16]int64 {
+	nextPolymers := make(map[uint16]int64)
+	for pair, v := range polymers {
+		monomer, ok := transformations[pair]
 		if !ok {
 			panic("I don't know how to deal with rejection :(")
 		}
-		prePair := string([]byte{k[0], monomer})
-		postPair := string([]byte{monomer, k[1]})
+		prePair := uint16(uint8(pair>>8))<<8 + uint16(monomer)
+		postPair := uint16(monomer)<<8 + uint16(uint8(pair))
 		nextPolymers[prePair] += v
 		nextPolymers[postPair] += v
 	}
 	return nextPolymers
 }
 
-func doubleCountMonomersButFixItToo(polymers map[string]int) map[uint8]int {
-	monomers := make(map[uint8]int)
+func doubleCountMonomersButFixItToo(polymers map[uint16]int64) map[uint8]int64 {
+	monomers := make(map[uint8]int64)
 	for k, v := range polymers {
-		monomers[k[0]] += v
-		monomers[k[1]] += v
+		monomers[uint8(k>>8)] += v
+		monomers[uint8(k)] += v
 	}
 	for k, _ := range monomers {
 		monomers[k] /= 2
@@ -67,48 +69,7 @@ func doubleCountMonomersButFixItToo(polymers map[string]int) map[uint8]int {
 	return monomers
 }
 
-func polymerize(chain string, transformations map[string]uint8) string {
-	sb := strings.Builder{}
-	for i := 0; i < len(chain)-1; i++ {
-		pair := chain[i : i+2]
-		monomer, ok := transformations[pair]
-		if !ok {
-			panic("I don't know how to deal with rejection :(")
-		}
-		sb.WriteByte(chain[i])
-		sb.WriteByte(monomer)
-	}
-	sb.WriteByte(chain[len(chain)-1]) // last char
-	return sb.String()
-}
-
-func countMonomers(chain string) map[rune]int {
-	monomers := make(map[rune]int)
-	for _, monomer := range chain {
-		monomers[monomer]++
-	}
-	return monomers
-}
-
-func ezMode(chain string, transformations map[string]uint8, steps int, ch chan<- int) {
-	for ; steps > 0; steps-- {
-		chain = polymerize(chain, transformations)
-	}
-	monomers := countMonomers(chain)
-	max := 0
-	min := math.MaxInt32
-	for _, v := range monomers {
-		if v > max {
-			max = v
-		}
-		if v < min {
-			min = v
-		}
-	}
-	ch <- max - min
-}
-
-func hardMode(chain string, transformations map[string]uint8, steps int, ch chan<- int) {
+func ezMode(chain string, transformations map[uint16]uint8, steps int, ch chan<- int64) {
 	polymers := countPolymers(chain)
 	for ; steps > 0; steps-- {
 		polymers = polymerizeButWithDP(polymers, transformations)
@@ -117,8 +78,8 @@ func hardMode(chain string, transformations map[string]uint8, steps int, ch chan
 	// first and last monomers aren't double counted
 	monomers[chain[0]]++
 	monomers[chain[len(chain)-1]]++
-	max := 0
-	min := math.MaxInt64
+	var max int64 = 0
+	var min int64 = math.MaxInt64
 	for _, v := range monomers {
 		if v > max {
 			max = v
@@ -132,11 +93,11 @@ func hardMode(chain string, transformations map[string]uint8, steps int, ch chan
 
 func Go(fileName string, ch chan string) {
 	polymer, transformations := readInput(fileName)
-	ezChan := make(chan int)
-	hardChan := make(chan int)
+	ezChan := make(chan int64)
+	hardChan := make(chan int64)
 
 	go ezMode(polymer, transformations, 10, ezChan)
-	go hardMode(polymer, transformations, 40, hardChan)
+	go ezMode(polymer, transformations, 40, hardChan)
 
 	ch <- fmt.Sprintln("Extended Polymerization")
 	ch <- fmt.Sprintf("  ezMode: %d\n", <-ezChan)
